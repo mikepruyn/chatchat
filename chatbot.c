5ccd38ba9d162756c11a90dc933361a2ee284811
mikepruyn <45433442+mikepruyn@users.noreply.github.com>
2018-12-09 17:55:46 -0500

    Update chatbot.c

--- chatbot.c
+++ chatbot.c
@@ -12,9 +12,11 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <chatbot.h>
 
-/* Simplifies calls to bind(), connect(), and accept() */
+#define MAXUSERS 100
+#define MAXROOMS 100
+
+ /* Simplifies calls to bind(), connect(), and accept() */
 typedef struct sockaddr SA;
 
 /* Max text line length */
@@ -32,214 +34,273 @@ int msgi = 0;
 // A lock for the message buffer.
 pthread_mutex_t lock;
 
+
+struct User {
+	char nickname[30];
+	int sockfd;
+};
+
+struct Room {
+	char name[30];
+	struct User * user_list[MAXUSERS];
+	int users; 
+};
+
+struct Room *create_room(char *name) {
+	struct Room *newRoom = malloc(sizeof(struct Room));
+	strcpy(newRoom->name, name);
+	newRoom->users = 0;
+	return newRoom;
+}
+
+struct User *create_user(char *name, int connfd, struct Room* room) {
+	struct User *newUser = malloc(sizeof(struct User));
+	strcpy(newUser->nickname, name);
+	newUser->sockfd = connfd;
+	room->user_list[room->users] = newUser;
+	room->users++;
+	return newUser;
+}
+//returns 1 on successful delete, -1 otherwise
+int delete_user(int connfd, struct Room *room) {
+	for (int i = 0; i < room->users; i++) {
+		if (room->user_list[i]->sockfd == connfd) {
+			room->user_list[i] = room->user_list[room->users - 1];
+			room->users--;
+			return 1;
+		}
+	}
+	return -1;
+}
+
 // Initialize the message buffer to empty strings.
 void init_message_buf() {
-  int i;
-  for (i = 0; i < 20; i++) {
-    strcpy(message_buf[i], "");
-  }
+	int i;
+	for (i = 0; i < 20; i++) {
+		strcpy(message_buf[i], "");
+	}
 }
 
 // This function adds a message that was received to the message buffer.
 // Notice the lock around the message buffer.
 void add_message(char *buf) {
-  pthread_mutex_lock(&lock);
-  strncpy(message_buf[msgi % 20], buf, 50);
-  int len = strlen(message_buf[msgi % 20]);
-  message_buf[msgi % 20][len] = '\0';
-  msgi++;
-  pthread_mutex_unlock(&lock);
+	pthread_mutex_lock(&lock);
+	strncpy(message_buf[msgi % 20], buf, 50);
+	int len = strlen(message_buf[msgi % 20]);
+	message_buf[msgi % 20][len] = '\0';
+	msgi++;
+	pthread_mutex_unlock(&lock);
 }
 
 // Destructively modify string to be upper case
 void upper_case(char *s) {
-  while (*s) {
-    *s = toupper(*s);
-    s++;
-  }
+	while (*s) {
+		*s = toupper(*s);
+		s++;
+	}
 }
 
 // A wrapper around recv to simplify calls.
 int receive_message(int connfd, char *message) {
-  return recv(connfd, message, MAXLINE, 0);
+	return recv(connfd, message, MAXLINE, 0);
 }
 
 // A wrapper around send to simplify calls.
 int send_message(int connfd, char *message) {
-  return send(connfd, message, strlen(message), 0);
+	return send(connfd, message, strlen(message), 0);
 }
 
 // A predicate function to test incoming message.
 int is_list_message(char *message) { return strncmp(message, "-", 1) == 0; }
 
 int send_list_message(int connfd) {
-  char message[20 * 50] = "";
-  for (int i = 0; i < 20; i++) {
-    if (strcmp(message_buf[i], "") == 0) break;
-    strcat(message, message_buf[i]);
-    strcat(message, ",");
-  }
+	char message[20 * 50] = "";
+	for (int i = 0; i < 20; i++) {
+		if (strcmp(message_buf[i], "") == 0) break;
+		strcat(message, message_buf[i]);
+		strcat(message, ",");
+	}
 
-  // End the message with a newline and empty. This will ensure that the
-  // bytes are sent out on the wire. Otherwise it will wait for further
-  // output bytes.
-  strcat(message, "\n\0");
-  printf("Sending: %s", message);
+	// End the message with a newline and empty. This will ensure that the
+	// bytes are sent out on the wire. Otherwise it will wait for further
+	// output bytes.
+	strcat(message, "\n\0");
+	printf("Sending: %s", message);
 
-  return send_message(connfd, message);
+	return send_message(connfd, message);
 }
 
 int send_echo_message(int connfd, char *message) {
-  upper_case(message);
-  add_message(message);
-  return send_message(connfd, message);
+	upper_case(message);
+	add_message(message);
+	return send_message(connfd, message);
 }
 
 int process_message(int connfd, char *message) {
-  
-  if(strncmp(message, "\\", 1) == 0){
-      if(strncmp(message, "\\JOIN", 5) == 0){
-          //code
-      }
-      else if(strncmp(message, "\\ROOMS", 6) == 0){
-          //code
-      }
-      else if(strncmp(message, "\\LEAVE", 6) == 0){
-          //code
-      }
-      else if(strncmp(message, "\\WHO", 4) == 0){
-          //code
-      }
-      else if(strncmp(message, "\\HELP", 5) == 0){
-          //code
-      }
-      else if(strncmp(message, "\\JOIN", 5) == 0){
-          //code
-      }
-      else if(){
+	if (strncmp(message, "\\", 1) == 0) {
+		if (strncmp(message, "\\JOIN", 5) == 0) {
+			char *nickname = strtok(message + 5, " ");
+			char *room_name = strtok(NULL, " ");
+			int room_exists = 0;
+			/*for (each room in list of rooms) {
+				if (room->name == room_name) {
+					create_user(nickname, connfd, room);
+					room_exists = 1;
+				}
+			}*/
+			if (!room_exists) {
+				struct Room *newRoom = create_room(room_name);
+				create_user(nickname, connfd, newRoom);
+			}
+			send_message(connfd, room_name);
+		}
+		else if (strncmp(message, "\\ROOMS", 6) == 0) {
+			char room_list[MAXROOMS * 32];
+			/*for (each room in list of rooms) {
+				strcat(room_list, room->name);
+			}*/
+			send_message(connfd, room_list);
+			
+		}
+		else if (strncmp(message, "\\LEAVE", 6) == 0) {
+			/*for (each room in list of rooms) {
+				if (delete_user(connfd, room) == 1) {
+					send_message(connfd, "GOODBYE");
+				}
+			}*/
+			send_message(connfd, "GOODBYE");
+		}
+		else if (strncmp(message, "\\WHO", 4) == 0) {
+			//code
+		}
+		else if (strncmp(message, "\\HELP", 5) == 0) {
+			//code
+		}
+		else {
+			//command not recognized
+		}
+	}
+	else {
 
-      }
-      else{
-          cout << "\"" << message << "\"" << "command not recognized."
-      }
-  }
+	}
 
-  if (is_list_message(message)) {
-    printf("Server responding with list response.\n");
-    return send_list_message(connfd);
-  } else {
-    printf("Server responding with echo response.\n");
-    return send_echo_message(connfd, message);
-  }
+	if (is_list_message(message)) {
+		printf("Server responding with list response.\n");
+		return send_list_message(connfd);
+	}
+	else {
+		printf("Server responding with echo response.\n");
+		return send_echo_message(connfd, message);
+	}
 }
 
 // The main function that each thread will execute.
 void echo(int connfd) {
-  size_t n;
+	size_t n;
 
-  // Holds the received message.
-  char message[MAXLINE];
+	// Holds the received message.
+	char message[MAXLINE];
 
-  while ((n = receive_message(connfd, message)) > 0) {
-    message[n] = '\0';  // null terminate message (for string operations)
-    printf("Server received message %s (%d bytes)\n", message, (int)n);
-    n = process_message(connfd, message);
-  }
+	while ((n = receive_message(connfd, message)) > 0) {
+		message[n] = '\0';  // null terminate message (for string operations)
+		printf("Server received message %s (%d bytes)\n", message, (int)n);
+		n = process_message(connfd, message);
+	}
 }
 
 // Helper function to establish an open listening socket on given port.
 int open_listenfd(int port) {
-  int listenfd;    // the listening file descriptor.
-  int optval = 1;  //
-  struct sockaddr_in serveraddr;
+	int listenfd;    // the listening file descriptor.
+	int optval = 1;  //
+	struct sockaddr_in serveraddr;
 
-  /* Create a socket descriptor */
-  if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) return -1;
+	/* Create a socket descriptor */
+	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) return -1;
 
-  /* Eliminates "Address already in use" error from bind */
-  if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval,
-                 sizeof(int)) < 0)
-    return -1;
+	/* Eliminates "Address already in use" error from bind */
+	if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval,
+		sizeof(int)) < 0)
+		return -1;
 
-  /* Listenfd will be an endpoint for all requests to port
-     on any IP address for this host */
-  bzero((char *)&serveraddr, sizeof(serveraddr));
-  serveraddr.sin_family = AF_INET;
-  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serveraddr.sin_port = htons((unsigned short)port);
-  if (bind(listenfd, (SA *)&serveraddr, sizeof(serveraddr)) < 0) return -1;
+	/* Listenfd will be an endpoint for all requests to port
+	   on any IP address for this host */
+	bzero((char *)&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons((unsigned short)port);
+	if (bind(listenfd, (SA *)&serveraddr, sizeof(serveraddr)) < 0) return -1;
 
-  /* Make it a listening socket ready to accept connection requests */
-  if (listen(listenfd, LISTENQ) < 0) return -1;
-  return listenfd;
+	/* Make it a listening socket ready to accept connection requests */
+	if (listen(listenfd, LISTENQ) < 0) return -1;
+	return listenfd;
 }
 
 // thread function prototype as we have a forward reference in main.
 void *thread(void *vargp);
 
 int main(int argc, char **argv) {
-  // Check the program arguments and print usage if necessary.
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(0);
-  }
+	// Check the program arguments and print usage if necessary.
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s <port>\n", argv[0]);
+		exit(0);
+	}
 
-  // initialize message buffer.
-  init_message_buf();
+	// initialize message buffer.
+	init_message_buf();
 
-  // Initialize the message buffer lock.
-  pthread_mutex_init(&lock, NULL);
+	// Initialize the message buffer lock.
+	pthread_mutex_init(&lock, NULL);
 
-  // The port number for this server.
-  int port = atoi(argv[1]);
+	// The port number for this server.
+	int port = atoi(argv[1]);
 
-  // The listening file descriptor.
-  int listenfd = open_listenfd(port);
+	// The listening file descriptor.
+	int listenfd = open_listenfd(port);
 
-  // The main server loop - runs forever...
-  while (1) {
-    // The connection file descriptor.
-    int *connfdp = malloc(sizeof(int));
+	// The main server loop - runs forever...
+	while (1) {
+		// The connection file descriptor.
+		int *connfdp = malloc(sizeof(int));
 
-    // The client's IP address information.
-    User user;
+		// The client's IP address information.
+		struct sockaddr_in clientaddr;
 
-    // Wait for incoming connections.
-    socklen_t clientlen = sizeof(struct sockaddr_in);
-    *connfdp = accept(listenfd, (SA *)&clientaddr, &clientlen);
+		// Wait for incoming connections.
+		socklen_t clientlen = sizeof(struct sockaddr_in);
+		*connfdp = accept(listenfd, (SA *)&clientaddr, &clientlen);
 
-    /* determine the domain name and IP address of the client */
-    struct hostent *hp =
-        gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
-                      sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+		/* determine the domain name and IP address of the client */
+		struct hostent *hp =
+			gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
+				sizeof(clientaddr.sin_addr.s_addr), AF_INET);
 
-    // The server IP address information.
-    char *haddrp = inet_ntoa(clientaddr.sin_addr);
+		// The server IP address information.
+		char *haddrp = inet_ntoa(clientaddr.sin_addr);
 
-    // The client's port number.
-    unsigned short client_port = ntohs(clientaddr.sin_port);
+		// The client's port number.
+		unsigned short client_port = ntohs(clientaddr.sin_port);
 
-    printf("server connected to %s (%s), port %u\n", hp->h_name, haddrp,
-           client_port);
+		printf("server connected to %s (%s), port %u\n", hp->h_name, haddrp,
+			client_port);
 
-    // Create a new thread to handle the connection.
-    pthread_t tid;
-    pthread_create(&tid, NULL, thread, connfdp);
-  }
+		// Create a new thread to handle the connection.
+		pthread_t tid;
+		pthread_create(&tid, NULL, thread, connfdp);
+	}
 }
 
 /* thread routine */
 void *thread(void *vargp) {
-  // Grab the connection file descriptor.
-  int connfd = *((int *)vargp);
-  // Detach the thread to self reap.
-  pthread_detach(pthread_self());
-  // Free the incoming argument - allocated in the main thread.
-  free(vargp);
-  // Handle the echo client requests.
-  echo(connfd);
-  printf("client disconnected.\n");
-  // Don't forget to close the connection!
-  close(connfd);
-  return NULL;
+	// Grab the connection file descriptor.
+	int connfd = *((int *)vargp);
+	// Detach the thread to self reap.
+	pthread_detach(pthread_self());
+	// Free the incoming argument - allocated in the main thread.
+	free(vargp);
+	// Handle the echo client requests.
+	echo(connfd);
+	printf("client disconnected.\n");
+	// Don't forget to close the connection!
+	close(connfd);
+	return NULL;
 }
